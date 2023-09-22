ada24a13eb28ac4a505d8833186a3a3ae3fc60b1
Hidomen <78243964+Hidomen@users.noreply.github.com>
2023-09-22 17:48:44 +0300

    Add files via upload

---
+++ questions.py
@@ -0,0 +1,17 @@
+# database
+
+q_list = [ # [level of question, "question", "a", "b", "c", "d", "right answer"
+    [1, "What is the smallest unit in a computer's memory","Byte","Dot","Chip","Bit","D"],
+    [1, "Which part of its body does a woodpecker use for pecking","beak","tail","wing", "foot", "A"],
+    [2, "Which word beginning with 'T' can go before 'table', 'coat', and 'stile'", "Top", "Turn", "Tea", "Times", "B"],
+    [3, "Which duke gave his name to a type of protective boots", "York", "Ellington", "Marlbrough", "Wellington", "D"],
+    [4, "In which game might you 'check out' with a 'double top'", "Cricket", "Golf", "Snooker", "Darts", "D"],
+    [5, "Who won the Best Actor Oscar in 1992 for his role in 'Scent of a Woman'", "Al Pacino", "Jeremy Irons", "Michael Douglas", "Gene Hackman", "A"],
+    [6, "What is the more common name for a 'lexicon'", "Travel Guide", "Psalm Book", "Dictionary", "Autobiography", "C"],
+    [6, "The 'Biathlon' combines cross-country skiing with which other sport", "Rifle Shooting", "Ice Skating", "Archery", "Canoeing", "A"],
+    [7, "Which car manufacturer produces a model called 'Polo'", "Renault", "Subaru", "Volkswagen", "Honda", "C"],
+    [7, "For what form of entertainment was Harry Houdini famous", "Ventriloquism", "Tightrope walking", "Conjuring", "Escapology", "D"],
+    [8, "In the film 'Witness', Harrison Ford hides out among which religious group", "Jehovah's Witnesses", "The Amish", "Mennonites", "Quakers", "B"],
+    [9, " Which was the first stately home to open its doors to the public", "Beaulieu", "Longleat", "Wilton House", "Blenheim Palace", "C"],
+    [10, "Which country is sandwiched between Ghana and Benin", "Togo", "Equitorial Guinea", "Cote d'Ivoire", "Gabon", "A"]
+]
